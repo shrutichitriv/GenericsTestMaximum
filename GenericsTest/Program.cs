bf3e764d9a72ad18eae9f242ece2ff653fdae67c
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-16 22:54:24 +0530

    [Shruti] Initializing test maximum program

---
+++ GenericsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMaximum maxValue = new FindMaximum();
+
+            int intResGen = maxValue.GetMaximun<int>(10, 78, 90);
+            Console.WriteLine("Maximum Interger Value is :" + intResGen);
+            double doubleResGen = maxValue.GetMaximun<double>(11.4, 89.67, 98.5);
+            Console.WriteLine("Maximum Double Value is :" + doubleResGen);
+            string stringResGen = maxValue.GetMaximun<string>("456", "946", "657");
+            Console.WriteLine("Maximum String is :" + stringResGen);
+        }
+    }
+}

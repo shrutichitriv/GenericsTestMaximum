a954f699bcf043fd6657b4ae2049de18203ae124
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-17 22:35:56 +0530

    [Shruti] Given three floats find the maximum

--- GenericsTest/FindMaximum.cs
+++ GenericsTest/FindMaximum.cs
@@ -30,5 +30,28 @@ namespace GenericsTest
             }
             return firstValue;
         }
+        public static double GetMaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+               firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
     }
 }
+
--- GenericsTest/Program.cs
+++ GenericsTest/Program.cs
@@ -9,6 +9,8 @@
             int result = FindMaximum.GetMaximumIntNumber(95, 150, 65);
             Console.WriteLine($"The maxumum number between three integer is : {result}");
 
+            double doubleResult = FindMaximum.GetMaximumFloatNumber(11.5, 11.6, 11.7);
+            Console.WriteLine($"The maxumum number between three integer is : {doubleResult}");
 
         }
     }
